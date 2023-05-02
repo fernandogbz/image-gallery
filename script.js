da4c2787ca8b5f087bd7d69cb1d0307028c9a98a
Fernando Jimenez <fgjv06@gmail.com>
2023-05-02 04:57:47 -0400

    JS - Later, we'll increment the currentPage on load more button click

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const apiKey = "lm6LXRXyUDilPNMvBkDX5ac8YpzOTWERwRwswvnHNP0mGV1auPQ6F1KL";
 // We'll load 15 images on every API call
 const perPage = 15;
+// Later, we'll increment the currentPage on load more button click
+let = currentPage = 1;
 
 
-getImages(`https://api.pexels.com/v1/curated?per_page=1`);
+getImages(`https://api.pexels.com/v1/curated?page=${currentPage}&per_page=${perPage}`);
