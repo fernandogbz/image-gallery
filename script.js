04d6904e63a745c32af1cdf2cc356fe450782148
Fernando Jimenez <fgjv06@gmail.com>
2023-05-02 05:10:14 -0400

    JS - generateHTML(IMAGES)

--- script.js
+++ script.js
@@ -5,12 +5,27 @@ const perPage = 15;
 // Later, we'll increment the currentPage on load more button click
 let = currentPage = 1;
 
+const generateHTML = (images) => {
+  images.map(img =>
+    `<li class="card">
+    <img src="${img.src.large2x}" alt="img" />
+    <div class="details">
+      <div class="photographer">
+        <i class="uil uil-camera"></i>
+        <span>${img.photographer}</span>
+      </div>
+      <button><i class="uil uil-import"></i></button>
+    </div>
+  </li>`
+    ).join("");
+}
+
 const getImages = (apiURL) => {
   // Fetching images by API call with authorization header
   fetch(apiURL, {
     headers: {Authorization: apiKey}
   }).then(res => res.json()).then(data =>{
-    console.log(data);
+    generateHTML(data.photos);
   })
 }
 
