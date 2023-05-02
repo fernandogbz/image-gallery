60cb5c584d70b9441a275924413558a9587e5f58
Fernando Jimenez <fgjv06@gmail.com>
2023-05-02 06:19:20 -0400

    JS - getting lightbox

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const imagesWrapper = document.querySelector(".images");
 const loadMoreBtn = document.querySelector(".load-more");
 const searchInput = document.querySelector(".search-box input");
+const lightbox = document.querySelector(".lightbox");
 
 // API key, paginations, searchTerm variables
 const apiKey = "lm6LXRXyUDilPNMvBkDX5ac8YpzOTWERwRwswvnHNP0mGV1auPQ6F1KL"; // Getting apiKey
