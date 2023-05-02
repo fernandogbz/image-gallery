0f35226aa72a9cf592f59af9db7dfd8acd74c6ef
Fernando Jimenez <fgjv06@gmail.com>
2023-05-02 04:54:15 -0400

    JS - We'll load 15 images on every API call

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Getting apiKey
 const apiKey = "lm6LXRXyUDilPNMvBkDX5ac8YpzOTWERwRwswvnHNP0mGV1auPQ6F1KL";
+// We'll load 15 images on every API call
+const perPage = 15;
+
 
 getImages(`https://api.pexels.com/v1/curated?per_page=1`);
